7772ebdf425b34b756bba66b2a590c844301031c
SpencerHCoding <9690536@graniteschools.org>
2023-12-10 11:03:35 -0700

    <mkl;sdfh>

--- node.js
+++ node.js
@@ -1,1 +1,7 @@
 document.body.innerHTML = "Hello my name is Spencer!";
+var btn = document.createElement("BUTTON");
+btn.innerHTML = "CLICK ME";
+btn.onclick = function() {
+  alert("Welcome to SpenncerHCoding and xRADxRYANx's website!");
+}
+document.body.appendChild(btn);
